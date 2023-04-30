19c457162dc9a4ffc19f2f5638debc7debcd4ffd
401lrx <1716318413@qq.com>
2023-04-30 14:30:24 +0800

    备份脚本更改为使用ini配置，暂时去除备份脚本中硬盘检查和发邮件的功能

--- source/systools/backup_disk.sh
+++ source/systools/backup_disk.sh
@@ -1,103 +1,139 @@
 #!/bin/bash
+# working in prefix/tools/systools/backup_disk.sh
+# default conf file is prefix/config/backup_disk.ini
+# default log file is prefix/log/backup_disk/[nowtime].log
 
 source /etc/profile
-
 cd `dirname $0`
 
+backup_time=`date "+%Y%m%d_%H"`
 usr=`whoami`
 if [[ "$usr" != "root" ]];then
-        echo please do script in root.
-        exit -1
-fi
-
-if [ $# -lt 1 ];then
-	echo "$0 backdisk(/dev/sda1)"
+	echo please do script in root.
 	exit 1
 fi
-disk=$1
 
-now=`date "+%Y%m%d_%H"`
+backup_conf=../../config/backup_disk.ini
+if [ $# -ge 1 ];then
+	backup_conf=$1
+fi
+if [ ! -f $backup_conf ];then
+	echo conf $backup_conf do not exist.
+	exit 1
+fi
+bash parseini.sh check $backup_conf || exit 1
 
-# 准备备份文件夹
-backup_root=`pwd`/backup
-log_path=${backup_root}/log
-log_file=$log_path/$now.log
-src_path=${backup_root}/src_disk
-dst_path=${backup_root}/backup_disk
-info_file=${backup_root}/rsynclist.txt
+log_path=$(bash parseini.sh var $backup_conf "common" "log_path")
+if [[ "x$log_path" == "x" ]]; then
+	log_path=../../log/backup_disk
+fi
+log_file=${log_path}/${backup_time}.log
+mkdir -p -m 755 $log_path
 
-if [ ! -d "${backup_root}" ];then
-        mkdir ${backup_root}
-        chmod 777 ${backup_root}
-fi
+# 准备临时文件夹
+work_dir=`pwd`/__backup_tmp
+work_from=${work_dir}/from
+work_to=${work_dir}/to
 
-if [ ! -d "${log_path}" ];then
-        mkdir ${log_path}
-        chmod 777 ${log_path}
-fi
-if [ ! -d "${src_path}" ];then
-        mkdir ${src_path}
-        chmod 777 ${src_path}
-fi
-if [ ! -d "${dst_path}" ];then
-        mkdir ${dst_path}
-        chmod 777 ${dst_path}
-fi
+function backuplog
+{
+	local timenow=`date +'%Y-%m-%d %H:%M:%S'`
+	while read line || [[ -n ${line} ]];do
+		echo "$timenow|"$line
+		echo "$timenow|"$line >> $log_file
+		echo "$timenow|"$line >> $work_dir/maillog.log
+	done < <(echo "$1")
+}
+
+function errorexit
+{
+	umount $work_from 2>/dev/null
+	umount $work_to 2>/dev/null
+	rm -rf $work_dir
+	exit 1
+}
+
+mkdir -p -m 777 $work_from
+mkdir -p -m 777 $work_to
 
 # 开始备份
-echo "=================begin backup===============" >> $log_file
-echo "begin time:`date`" >> $log_file
+backuplog "==================================================="
+backuplog "begin backup"
+for section in $(bash parseini.sh section $backup_conf); do
+	if [[ "$section" == "common" ]];then continue; fi
+	# 准备目录
+	from_type=$(bash parseini.sh var $backup_conf $section "from_type")
+	case $from_type in
+		smb)
+			from_address=$(bash parseini.sh var $backup_conf $section "from_address")
+			from_dir=$(bash parseini.sh var $backup_conf $section "from_dir")
+			user=$(bash parseini.sh var $backup_conf $section "user")
+			passwd=$(bash parseini.sh var $backup_conf $section "passwd")
+			if [[ "x$from_address" == "x" ]];then
+				backuplog "backup conf error|$section|from_address|$from_address"
+				errorexit
+			fi
+			mount //$from_address/$from_dir $work_from -t cifs --options "username=$user,password=$passwd,dir_mode=0777,file_mode=0777,vers=3.0"
+			if [ $? -ne 0 ];then
+				backuplog "mount smb error|$section|from|//$from_address/$from_dir"
+				errorexit
+			else
+				backuplog "mount smb //$from_address/$from_dir to $work_from"
+				real_from=$work_from
+			fi
+		;;
+		*)
+		 	backuplog "backup conf error|$section|from_type|$from_type"
+			errorexit
+		;;
+	esac
+	to_type=$(bash parseini.sh var $backup_conf $section "to_type")
+	case $to_type in
+		disk)
+			to_address=$(bash parseini.sh var $backup_conf $section "to_address")
+			to_dir=$(bash parseini.sh var $backup_conf $section "to_dir")
+			if [[ "x$to_address" == "x" ]];then
+				backuplog "backup conf error|$section|to_address|$to_address"
+				errorexit
+			fi
+			mount $to_address $work_to
+			if [ $? -ne 0 ];then
+				backuplog "mount disk error|$section|to|$to_address"
+				errorexit
+			else
+				backuplog "mount disk $to_address to $work_to"
+				real_to=$work_to/$to_dir
+			fi
+		;;
+		*)
+			backuplog "backup conf error|$section|to_type|$to_type"
+			errorexit
+		;;
+	esac
 
-backupDir=(
-"//192.168.5.211/PublicData"
-"//192.168.5.233/docker"
-"//192.168.5.211/photo"
-)
+	# 开始备份
+	exclude_cmd=""
+	for exclude_one in $(bash parseini.sh var $backup_conf $section "exclude" | sed "s/,/ /g");do
+		exclude_cmd="$exclude_cmd --exclude '$exclude_one'"
+	done
+	backuplog $(rsync -hvaiEW --delete $exclude_cmd $real_from/ $real_to)
 
-echo "now:" $now > ${info_file}
-
-echo "mount backup disk....." >> $log_file
-mount ${disk} ${dst_path}
-
-for src_dir in ${backupDir[@]};do
-        dirname=`basename ${src_dir}`
-        echo "mount src:${src_dir}....." >> $log_file
-        mount ${src_dir} ${src_path} -t cifs --options "username=backup,password=backup,dir_mode=0777,file_mode=0777,vers=3.0"
-
-        echo "rsync ${src_path} ${dst_path}/${dirname}" >> $log_file
-        echo "--------------------------------------------------------------------------" >> ${info_file}
-        rsync -hvaiEW --delete --exclude '#recycle' --exclude 'Download' --exclude 'nobackup' --exclude 'Thumbs.db' ${src_path}/ ${dst_path}/${dirname} >> ${info_file}
-
-        echo "umount src:${src_dir}....." >> $log_file
-        umount $src_path
+	# 卸载目录
+	case $from_type in
+		smb)
+			umount $work_from
+			backuplog "umount smb //$from_address/$from_dir"
+		;;
+	esac
+	case $to_type in
+		disk)
+			umount $work_to
+			backuplog "umount disk $to_address"
+		;;
+	esac
 done
 
-echo "umount backup disk....." >> $log_file
-umount ${dst_path}
+backuplog "end backup"
 
-# 额外检测一下硬盘健康度
-smartresult=`smartctl -H ${disk} | sed -n "s/\(.\+\)\(result: \)\(.\+\)/\3/p"`
-if [[ "x${smartresult}" != "xPASSED" ]];then
-        subject="[!!! ATTENTION !!!] disk ${disk} may has error !"
-        msg="${disk} not healthy, pls check!!!
-disk info:
-`smartctl -i ${disk}`
-
-disk health:
-`smartctl -H ${disk}`
-
-disk info2:
-`smartctl -A ${disk}`
-
-disk error:
-`smartctl -l error ${disk}`"
-        mysendmail send -t "1716318413@qq.com" -m "${msg}" -s "${subject}"
-fi
-
-hdparm -Y ${disk}
-
-# 发结果邮件
-mysendmail send -t "1716318413@qq.com" -m "每周备份完成，请检查备份情况。" -s "[[每周备份提醒]] ${now}" -f "${info_file}"
-
-echo "end time:`date`" >> $log_file
-echo "=================end backup===============" >> $log_file
+# 删除临时目录
+rm -rf $work_dir
--- source/systools/parseini.sh
+++ source/systools/parseini.sh
@@ -16,13 +16,13 @@ EOF
 function check_ini_syntax()
 {
     if [ ! -f $1 ];then
-		echo "[error] file not exists."
-        return 1
+        echo "[error] file not exists."
+        exit 1
     fi
 
-	linenum=0
+    linenum=0
     while read oline || [[ -n ${line} ]];do
-		linenum=$(( $linenum + 1 ))
+        linenum=$(( $linenum + 1 ))
         # 去除注释
         line=${oline%%#*}
         # 去除左右空格
@@ -32,107 +32,107 @@ function check_ini_syntax()
         # 判断类型
         beginc=${line:0:1}
         case $beginc in
-			"[")
-				if [[ ! "$line" =~ ^\[[[:alnum:]]+\]$ ]];then
-					echo "[error] line $linenum: $oline"
-					echo "[info] Section name must only consist of numbers and letters, without spaces."
-					return 1
-				fi
-			;;
-			*)
-				if [[ ! "$line" =~ ^[[:alnum:]_]+\=.+$ ]];then
-					echo "[error] line $linenum: $oline"
-					echo "[info] There should be no spaces around the equal sign. Variable names contain only alphanumeric characters and underscores."
-					return 1
-				fi
-			;;
-		esac
+            "[")
+                if [[ ! "$line" =~ ^\[[[:alnum:]]+\]$ ]];then
+                    echo "[error] line $linenum: $oline"
+                    echo "[info] Section name must only consist of numbers and letters, without spaces."
+                    exit 1
+                fi
+            ;;
+            *)
+                if [[ ! "$line" =~ ^[[:alnum:]_]+\=.*$ ]];then
+                    echo "[error] line $linenum: $oline"
+                    echo "[info] There should be no spaces around the equal sign. Variable names contain only alphanumeric characters and underscores."
+                    exit 1
+                fi
+            ;;
+        esac
     done < $1
 }
 
 function get_all_section
 {
-	check_ini_syntax $1 || exit 1
     while read oline || [[ -n ${line} ]];do
-		# 去除注释
+        # 去除注释
         line=${oline%%#*}
         # 去除左右空格
         line=$(echo $line)
-		# 跳过空行以及非段名定义的行
-        if [ "x" == "x${line}" ] || [[ $line =~ ^[[:alnum:]_]+\=.+$ ]]; then continue; fi
+        # 跳过空行以及非段名定义的行
+        if [ "x" == "x${line}" ] || [[ $line =~ ^[[:alnum:]_]+\=.*$ ]]; then continue; fi
 
-		echo $line | awk -F '[][]' '{print $2 }'
-	done < $1
+        echo $line | awk -F '[][]' '{print $2 }'
+    done < $1
 }
 
 function get_section_all_var
 {
-	check_ini_syntax $1 || exit 1
-	insection=0
+    insection=0
     while read oline || [[ -n ${line} ]];do
-		# 去除注释
+        # 去除注释
         line=${oline%%#*}
-		# 去除左右空格
+        # 去除左右空格
         line=$(echo $line)
-		# 跳过空行
-		if [ "x" == "x${line}" ]; then continue; fi
+        # 跳过空行
+        if [ "x" == "x${line}" ]; then continue; fi
 
-		# 在段内，碰到下一个段名，返回
-		if [ $insection -eq 1 ] && [[ $line =~ ^\[[[:alnum:]]+\]$ ]]; then return; fi
-		# 不在段内，直到找到需要的段名
-		if [ $insection -eq 0 ] && [[ "$line" != "[$2]" ]]; then continue; fi
-		# 进入段内，设置flag，跳过段名行
-		if [ $insection -eq 0 ]; then insection=1; continue; fi
-		echo $line | awk -F= '{print $1 " " $2}'
-	done < $1
+        # 在段内，碰到下一个段名，返回
+        if [ $insection -eq 1 ] && [[ $line =~ ^\[[[:alnum:]]+\]$ ]]; then return; fi
+        # 不在段内，直到找到需要的段名
+        if [ $insection -eq 0 ] && [[ "$line" != "[$2]" ]]; then continue; fi
+        # 进入段内，设置flag，跳过段名行
+        if [ $insection -eq 0 ]; then insection=1; continue; fi
+        echo $line | awk -F= '{print $1 " " $2}'
+    done < $1
 }
 
 function get_section_var
 {
-	check_ini_syntax $1 || exit 1
-	insection=0
+    insection=0
     while read oline || [[ -n ${line} ]];do
-		# 去除注释
+        # 去除注释
         line=${oline%%#*}
-		# 去除左右空格
+        # 去除左右空格
         line=$(echo $line)
-		# 跳过空行
-		if [ "x" == "x${line}" ]; then continue; fi
+        # 跳过空行
+        if [ "x" == "x${line}" ]; then continue; fi
 
-		# 在段内，碰到下一个段名，返回
-		if [ $insection -eq 1 ] && [[ $line =~ ^\[[[:alnum:]]+\]$ ]]; then return; fi
-		# 不在段内，直到找到需要的段名
-		if [ $insection -eq 0 ] && [[ "$line" != "[$2]" ]]; then continue; fi
-		# 进入段内，设置flag，跳过段名行
-		if [ $insection -eq 0 ]; then insection=1; continue; fi
-		echo $line | awk -F= '$1=="'$3'" {print $2}'
-	done < $1
+        # 在段内，碰到下一个段名，返回
+        if [ $insection -eq 1 ] && [[ $line =~ ^\[[[:alnum:]]+\]$ ]]; then return; fi
+        # 不在段内，直到找到需要的段名
+        if [ $insection -eq 0 ] && [[ "$line" != "[$2]" ]]; then continue; fi
+        # 进入段内，设置flag，跳过段名行
+        if [ $insection -eq 0 ]; then insection=1; continue; fi
+        echo $line | awk -F= '$1=="'$3'" {print $2}'
+    done < $1
 
 }
 
 if [ $# -lt 2 ];then 
-	usage
-	exit 1
+    usage
+    exit 1
 fi
 op=$1
 file=$2
 case $op in
-	check)
-		check_ini_syntax $file
-	;;
-	section)
-		if [ $# -ge 3 ];then
-			get_section_all_var $file $3
-		else
-			get_all_section $file
-		fi
-	;;
-	var)
-		if [ $# -lt 4 ];then
-			usage
-			exit 1
-		else
-			get_section_var $file $3 $4
-		fi
-	;;
+    check)
+        check_ini_syntax $file
+    ;;
+    section)
+        if [ $# -ge 3 ];then
+            get_section_all_var $file $3
+        else
+            get_all_section $file
+        fi
+    ;;
+    var)
+        if [ $# -lt 4 ];then
+            usage
+            exit 1
+        else
+            get_section_var $file $3 $4
+        fi
+    ;;
+    help)
+        usage
+    ;;
 esac	
