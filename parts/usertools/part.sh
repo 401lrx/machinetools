3cfd50dc0394686c565d8881d014a241f6c1c59c
401lrx <1716318413@qq.com>
2023-05-03 01:23:43 +0800

    增加usertools的clean功能

--- parts/usertools/part.sh
+++ parts/usertools/part.sh
@@ -9,168 +9,173 @@ config_path=config
 
 function usage
 {
-    cat << EOF
+	cat << EOF
 cur part: $partname
 usage: $0 op
-    op [install|clean|help]
+	op [install|clean|help]
 EOF
 }
 
 function create_user
 {
-    user=$2
-    checkuser=`cut -d: -f1 /etc/passwd | grep -e "^$user$"`
-    if [[ "x${checkuser}" != "x" ]];then
-        normalp "User $user exists"
-        return
-    fi
-    
-    useradd -m -d /home/$user -s /bin/bash $user
-    passwd $user
-    case $1 in
-        pve)
-            normalp "[!!!! ATTENTION !!!!] please continue manual configure user auth in web GUI /Datacenter/Permissions/Users"
-        ;;
-    esac
-    success "User ${user} create done!!! "
+	local user=$2
+	local checkuser=`cut -d: -f1 /etc/passwd | grep -e "^$user$"`
+	if [[ "x${checkuser}" != "x" ]];then
+		normalp "User $user exists"
+		return
+	fi
+	
+	useradd -m -d /home/$user -s /bin/bash $user
+	passwd $user
+	case $1 in
+		pve)
+			normalp "[!!!! ATTENTION !!!!] please continue manual configure user auth in web GUI /Datacenter/Permissions/Users"
+		;;
+	esac
+	success "User ${user} create done!!! "
 }
 
 function createcctool
 {
-    user=$2
-    checkuser=`cut -d: -f1 /etc/passwd | grep -e "^$user$"`
-    if [[ "x${checkuser}" == "x" ]];then
-        error "User $user not exists, cant create cctool"
-        return
-    fi
+	local user=$2
+	local checkuser=`cut -d: -f1 /etc/passwd | grep -e "^$user$"`
+	if [[ "x${checkuser}" == "x" ]];then
+		error "User $user not exists, cant create cctool"
+		return
+	fi
 
-    # create prefix/tools/mygotools
-    createPath $tools_path 777
-    createPath $install_path 755
-    yes 2>/dev/null | cp -Rf $MACHINE_INIT_WORK_DIR/source/usertools/* $MACHINE_INIT_PREFIX/$install_path/
-    chmod -R 755 $MACHINE_INIT_PREFIX/$install_path
-    createPath $config_path 777
-    success "toolsfunc config init done!!!"
+	# create prefix/tools/mygotools
+	createPath $tools_path 777
+	createPath $install_path 755
+	yes 2>/dev/null | cp -Rf $MACHINE_INIT_WORK_DIR/source/usertools/* $MACHINE_INIT_PREFIX/$install_path/
+	chmod -R 755 $MACHINE_INIT_PREFIX/$install_path
+	createPath $config_path 777
+	success "toolsfunc config init done!!!"
 
-    # create default in prefix/config
-    if [ ! -f $MACHINE_INIT_PREFIX/$config_path/toolsconfig.txt ];then
-        yes 2>/dev/null | cp -f $MACHINE_INIT_WORK_DIR/source/usertools/toolsconfig.txt $MACHINE_INIT_PREFIX/$config_path/
-    fi
-    if [ ! -f $MACHINE_INIT_PREFIX/$config_path/ssh_getpwd.sh ];then
-        yes 2>/dev/null | cp -f $MACHINE_INIT_WORK_DIR/source/usertools/ssh_getpwd.sh $MACHINE_INIT_PREFIX/$config_path/
-    fi
-    chmod +x $MACHINE_INIT_PREFIX/$config_path/ssh_getpwd.sh
+	# create default in prefix/config
+	if [ ! -f $MACHINE_INIT_PREFIX/$config_path/toolsconfig.txt ];then
+		yes 2>/dev/null | cp -f $MACHINE_INIT_WORK_DIR/source/usertools/toolsconfig.txt $MACHINE_INIT_PREFIX/$config_path/
+	fi
+	if [ ! -f $MACHINE_INIT_PREFIX/$config_path/ssh_getpwd.sh ];then
+		yes 2>/dev/null | cp -f $MACHINE_INIT_WORK_DIR/source/usertools/ssh_getpwd.sh $MACHINE_INIT_PREFIX/$config_path/
+	fi
+	chmod +x $MACHINE_INIT_PREFIX/$config_path/ssh_getpwd.sh
 
-    # edit user bashrc, add env
-    # delete old
-    sed -i '/^# config usertools begin/,/^# config usertools end/d' /home/${user}/.bashrc
-    # create new
-    echo '# config usertools begin
+	# edit user bashrc, add env
+	# delete old
+	sed -i '/^# config usertools begin/,/^# config usertools end/d' /home/${user}/.bashrc
+	# create new
+	echo '# config usertools begin
 toolfile='${MACHINE_INIT_PREFIX}/${install_path}'/toolsfunc.sh
 toolconfig='${MACHINE_INIT_PREFIX}/${config_path}/toolsconfig.txt'
 sshconfig='${MACHINE_INIT_PREFIX}/${config_path}/ssh_getpwd.sh'
 if [ -f ${toolfile} ];then
-        export TOOLS_CONFIG=${toolconfig}
-        export SSH_GETPWD=${sshconfig}
-        source ${toolfile}
-        select_env home
+		export TOOLS_CONFIG=${toolconfig}
+		export SSH_GETPWD=${sshconfig}
+		source ${toolfile}
+		select_env home
 fi
 # config usertools end' >> /home/${user}/.bashrc
 chown ${user}:${user} /home/${user}/.bashrc
 
-    # pve6 use bash_profile
-    case $1 in
-        pve)
-            echo '
+	# pve6 use bash_profile
+	case $1 in
+		pve)
+			echo '
 if [ -n "$BASH_VERSION" ]; then  
-    # include .bashrc if it exists  
-    if [ -f "$HOME/.bashrc" ]; then  
-        . "$HOME/.bashrc"  
-    fi  
+	# include .bashrc if it exists  
+	if [ -f "$HOME/.bashrc" ]; then  
+		. "$HOME/.bashrc"  
+	fi  
 fi' >> /home/${user}/.bash_profile
-            chown ${user}:${user} /home/${user}/.bash_profile
-        ;;
-    esac
+			chown ${user}:${user} /home/${user}/.bash_profile
+		;;
+	esac
 }
 
 function _install
 {
-    ostype=$(getostype)
+	local ostype=$(getostype)
 
-    # limit to create homecc now
-    if false ;then
-        while true; do
-            read -p "Create cc user, please enter user name:" username
-            case $username in
-                root) normalp "User can not be root"  ;;
-                *) break ;;
-            esac
-        done
-    else
-        username=homecc
-    fi
+	# limit to homecc now
+	local user="homecc"
+	if false ;then
+		while true; do
+			read -p "Create usertools, please enter user name:" user
+			case $user in
+				root) normalp "User can not be root"  ;;
+				*) break ;;
+			esac
+		done
+	fi
 
-    # create user
-    while true; do
-        read -p "Do you wish to create user ${username}(yes/no)?" yn
-        case $yn in
-            [Yy]* ) create_user $ostype $username; break;;
-            [Nn]* ) break;;
-            * ) normalp "Please answer yes or no";;
-        esac
-    done
+	# create user
+	while true; do
+		read -p "Do you wish to create user ${user}(yes/no)?" yn
+		case $yn in
+			[Yy]* ) create_user $ostype $user; break;;
+			[Nn]* ) break;;
+			* ) normalp "Please answer yes or no";;
+		esac
+	done
 
-    # create user tools
-    while true; do
-        read -p "Do you wish to create ${username} tools (yes/no)?" yn
-        case $yn in
-            [Yy]* ) createcctool $ostype $username; break;;
-            [Nn]* ) break;;
-            * ) normalp "Please answer yes or no";;
-        esac
-    done
+	# create user tools
+	while true; do
+		read -p "Do you wish to create ${user} tools (yes/no)?" yn
+		case $yn in
+			[Yy]* ) createcctool $ostype $user; break;;
+			[Nn]* ) break;;
+			* ) normalp "Please answer yes or no";;
+		esac
+	done
 }
 
 function _clean
 {
-    ostype=$(getostype)
-    case $ostype in
-        centos|pve|ubuntu)
-            normalp "$partname not support clean"
-        ;;
-        *)
-            error "$ostype not support now"
-        ;;
-    esac
+	local user="homecc"
+	# limit to homecc now
+	if false ;then
+		while true; do
+			read -p "Clean usertools, please enter user name:" user
+			break
+		done
+	fi
+
+	# delete prefix/tools/usertools
+	normalp "clean old usertools ..."
+	rm -rf $MACHINE_INIT_PREFIX/$install_path
+	# clean bashrc
+	normalp "clean /home/${user}/.bashrc ..."
+	sed -i '/^# config usertools begin/,/^# config usertools end/d' /home/${user}/.bashrc
 }
 
 if [[ $# -lt 1 ]];then
-    usage
-    exit 0
+	usage
+	exit 0
 fi
 
 op=$1
 case $op in
-    install)
-        while true; do
-            read -p "Do you wish to install ${partname}(yes/no)?" yn
-            case $yn in
-                [Yy]* ) _install; break;;
-                [Nn]* ) exit 100;;
-                * ) normalp "Please answer yes or no";;
-            esac
-        done
-    ;;
-    clean)
-        while true; do
-            read -p "Do you wish to clean ${partname}(yes/no)?" yn
-            case $yn in
-                [Yy]* ) _clean; break;;
-                [Nn]* ) exit 100;;
-                * ) normalp "Please answer yes or no";;
-            esac
-        done
-    ;;
-    help) usage;;
-    *) error "Unknow op:" $op;;
+	install)
+		while true; do
+			read -p "Do you wish to install ${partname}(yes/no)?" yn
+			case $yn in
+				[Yy]* ) _install; break;;
+				[Nn]* ) exit 100;;
+				* ) normalp "Please answer yes or no";;
+			esac
+		done
+	;;
+	clean)
+		while true; do
+			read -p "Do you wish to clean ${partname}(yes/no)?" yn
+			case $yn in
+				[Yy]* ) _clean; break;;
+				[Nn]* ) exit 100;;
+				* ) normalp "Please answer yes or no";;
+			esac
+		done
+	;;
+	help) usage;;
+	*) error "Unknow op:" $op;;
 esac
